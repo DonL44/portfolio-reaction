33a51b3257c18234f270661d0600056753c23ebc
Donovan <Donovan1478@yahoo.com>
2022-05-05 17:42:55 -0500

    edited app.css

---
+++ portfolio-reaction/src/components/About/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function About() {
+  return (
+    <section class="about">
+      <h1 id="about">I'm an About Me section</h1>
+      <p>Hi! This is Don L, and I'm attending Vanderbilt University Coding Boot Camp with the hopes of becoming a top-teerWeb Developer. I'm currently working full time at a law enforcement agency while trying to balance my studies.
+         I also worked on a Bachelor's degree in Pschology. While combining my current experience with all the experience I've gained throughout my wide range of work history I've developed the ambition to start up my own Business.
+         My goal is to create a service that provides people the opportunity to network and meet others. From there, my following goals are to create a safe and positive space for Children, to give them a outlet outside of school 
+         and home that promotes Diversity. I'm confident that by being a web developer I can use my skills to better promote my message. I am currently looking for a professional opportunity as a Web Developer. If my profile intrests 
+         you, please don't hesitate to contact me.
+      </p>      
+    </section>
+  );
+}
+
+export default About;
